23c91131e7ac2b390a07dd25e4d8b5c9ac3ffda5
Shiva <sandupatlashiva2@gmail.com>
2023-04-29 15:09:43 +0530

    1.0.0

--- test4.py
+++ test4.py
@@ -1,13 +1,12 @@
-import cv2
 import os
+import cv2
 import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import train
+
+import re
 import csv
 import gui
-import train
-import re
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
-
 
 statevars = ["T", "R"]
 
@@ -22,191 +21,175 @@ if (not name.isalpha() and name not in statevars):
 if (name == "T"):
     train.train()
 
-face_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-labels = []
-encoded_face_train = []
-images = []
-# Function to load images from a folder
+datalabels={}
+def prepare_dataset(path):
+    count = 0
+    labels, faces = [], []
+    for person in os.listdir(path):
+        person_path = os.path.join(path, person)
+        if os.path.isdir(person_path):
+            for img in os.listdir(person_path):
+                img_path = os.path.join(person_path, img)
+                face = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                face = cv2.resize(face, (100, 100))
+
+                faces.append(face)
+                count = count + 1
+                datalabels[count] = img.split('.')[0]
+                labels.append(count)
+    return np.array(faces), np.array(labels)
+
+def train_models(faces, labels):
+    lbph = cv2.face.LBPHFaceRecognizer_create()
+    eigen = cv2.face.EigenFaceRecognizer_create()
+    lbph.train(faces, labels)
+    eigen.train(faces, labels)
+    return lbph, eigen
+
+def save_models(lbph, eigen):
+    lbph.write('lbph_model.yml')
+    eigen.write('eigen_model.yml')
+
+def load_models():
+    lbph = cv2.face.LBPHFaceRecognizer_create()
+    eigen = cv2.face.EigenFaceRecognizer_create()
+    lbph.read('lbph_model.yml')
+    eigen.read('eigen_model.yml')
+    return lbph, eigen
+
+def evaluate_models(lbph, eigen, faces, labels):
+    lbph_preds, eigen_preds = [], []
+    for face in faces:
+        _1, lbph_pred = lbph.predict(face)
+        _, eigen_pred = eigen.predict(face)
+        cv2.putText(face, str(_1), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), 2)
+        cv2.putText(face, str(_), (80, 50), cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), 2)
+        cv2.imshow('face', face)
+        lbph_preds.append(_1)
+        eigen_preds.append(_)
+        print(_1,_, lbph_pred, eigen_pred)
+
+    lbph_accuracy = accuracy_score(labels, lbph_preds)
+    lbph_precision = precision_score(labels, lbph_preds, average='weighted')
+    lbph_recall = recall_score(labels, lbph_preds, average='weighted')
+    lbph_f1 = f1_score(labels, lbph_preds, average='weighted')
+
+    eigen_accuracy = accuracy_score(labels, eigen_preds)
+    eigen_precision = precision_score(labels, eigen_preds, average='weighted')
+    eigen_recall = recall_score(labels, eigen_preds, average='weighted')
+    eigen_f1 = f1_score(labels, eigen_preds, average='weighted')
+    
+    return lbph_accuracy, lbph_precision, lbph_recall, lbph_f1,eigen_accuracy,eigen_precision,eigen_recall,eigen_f1
 
 
-def load_images_from_folder(folder_path):
-    for label in os.listdir(folder_path):
-        label_folder = os.path.join(folder_path, label)
-        if os.path.isdir(label_folder):
-            for filename in os.listdir(label_folder):
-                img_path = os.path.join(label_folder, filename)
-                if img_path.endswith(".jpg") or img_path.endswith(".png"):
-                    img = cv2.imread(os.path.join(label_folder, filename))
-                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    labels.append(filename.split('.')[0])
-                    img1 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-                    if img is not None:
-                        images.append(img)
-    return images
+def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, thickness=2, colour=(0, 255, 0)):
+    size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+    x, y = point
+    cv2.rectangle(image, (x, y-size[1]), (x+size[0], y), colour, cv2.FILLED)
+    cv2.putText(image, label, point, font, font_scale,
+                (255, 255, 255), thickness)
 
 
-# Load the known faces and their labels
-known_faces = []
-known_labels = []
-true_labels = []
+def main():
+    confirmstate = "nan"
+    confirmstatenum = 0
+    dataset_path = './voters'
+    dataset_path = './test'
+    faces, labels = prepare_dataset(dataset_path)
+    testfaces, testlabels = prepare_dataset(dataset_path)
+    lbph, eigen = train_models(faces, labels)
+    save_models(lbph, eigen)
+    lbph, eigen = load_models()
 
 
-# Load training images and labels
-folder_path = "./voters"
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    cap = cv2.VideoCapture(0)
 
-def harcascade_model_load_images_from_folder(folder_path):
-    for label in os.listdir(folder_path):
-        label_folder = os.path.join(folder_path, label)
-        if os.path.isdir(label_folder):
-            for filename in os.listdir(label_folder):
-                img_path = os.path.join(label_folder, filename)
-                if img_path.endswith(".jpg") or img_path.endswith(".png"):
-                    img = cv2.imread(os.path.join(label_folder, filename))
-                    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-                    faces = face_cascade.detectMultiScale(img_gray, 1.3, 5)
-                    if len(faces) > 0:
-                        (x, y, w, h) = faces[0]
-                        encoding = cv2.resize(img_gray[y:y+h, x:x+w], (480, 480))
-                        known_faces.append(encoding)
-                        known_labels.append(filename.split('.')[0])
-    return known_faces
+    counting = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if(counting<=3):
+            try:
+                lbph_accuracy, lbph_precision, lbph_recall, lbph_f1,eigen_accuracy,eigen_precision,eigen_recall,eigen_f1 = evaluate_models(lbph, eigen, testfaces, testlabels)
+            except:
+                print("evaluate_models issue ")
+            counting = counting + 1
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-harcascade_imgs=harcascade_model_load_images_from_folder(folder_path)
+        for (x, y, w, h) in faces:
+            face_gray = gray[y:y+h, x:x+w]
+            face_gray_resized = cv2.resize(face_gray, (100, 100))
+
+            _1, lbph_pred = lbph.predict(face_gray_resized)
+            _, eigen_pred = eigen.predict(face_gray_resized)
+            lbph_label = datalabels[_1]
+            eigen_label = datalabels[_]
+
+            print(lbph_pred, eigen_pred) #120 3500
+            label_text = ""
+
+            if(lbph_pred<=120):
+                label_text = lbph_label
+                cv2.putText(frame, f'{lbph_label}', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            elif(eigen_pred<=3500):
+                label_text = eigen_label
+                cv2.putText(frame, f'{eigen_label}', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.putText(frame, f'labels : {_1}, {_}', (x, y-60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+            # lbph_accuracy, lbph_precision, lbph_recall, lbph_f1,eigen_accuracy,eigen_precision,eigen_recall,eigen_f1
+
+            label_text = re.sub(r'[\d_]+', '', label_text)
+            # print(label_text)
+
+            if (confirmstatenum >= 40):
+                        confirmstate = label_text
+                        # print(confirmstate)
+                        gui.callGUI(label_text)
+                        while True:
+                            if (gui.windowopenstate == 1):
+                                print("Waiting for person vote")
+                            else:
+                                break
+                        confirmstatenum = 0
+            else:
+
+                with open('votes.csv', 'r', newline='') as csvfile:
+                    # Create a writer object
+                    csvreader = csv.reader(csvfile)
+                    # Iterate over each row in the CSV file
+                    data = [row[1] for row in csvreader]
+                    # Check if an item exists in the list
+                    print(data)
+                    if label_text in data:
+                        print("Already voted")
+                        draw_label(frame, (x, y), label_text +
+                                " Already voted", colour=(0, 0, 255))
+                        confirmstatenum = 0
+                    else:
+                        print('Security check......')
+                        draw_label(frame, (x, y), label_text)
+                        confirmstatenum = confirmstatenum + 1
 
 
-images = load_images_from_folder(folder_path)
+        cv2.putText(frame, f'lbph Accuracy: {lbph_accuracy:.2f}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, f'lbph precision: {lbph_precision:.2f}', (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, f'lbph recall: {lbph_recall:.2f}', (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, f'lbph F1 Score: {lbph_f1:.2f}', (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-images = [cv2.resize(img, (420, 420)) for img in images]
+        cv2.putText(frame, f'eigen Accuracy: {eigen_accuracy:.2f}', (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, f'eigen precision: {eigen_precision:.2f}', (10, 180), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, f'eigen recall: {eigen_recall:.2f}', (10, 210), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+        cv2.putText(frame, f'eigen F1 Score: {eigen_f1:.2f}', (10, 240), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
-# Create a dictionary to map each label to an integer
-label_dict = {}
-for i, label in enumerate(set(labels)):
-    label_dict[label] = i
-
-
-def get_key(val, my_dict):
-    for key, value in my_dict.items():
-        if val == value:
-            return key
-
-    return "Value not found in dictionary"
-
-
-# Convert the labels to an array of integers
-labels = np.array([label_dict[label] for label in labels])
-
-# Create face recognizer and train it
-face_recognizer = cv2.face.EigenFaceRecognizer_create()
-face_recognizer.train(images, labels)
-lbph_recognizer = cv2.face.LBPHFaceRecognizer_create()
-lbph_recognizer.train(images, labels)
-
-face_recognizer.write('eigenface_model.xml')
-lbph_recognizer.write('lpfh_model.xml')
-
-
-face_recognizer.read('eigenface_model.xml')
-lbph_recognizer.read('lpfh_model.xml')
-
-
-# Initialize variables for calculating metrics and storing predicted labels
-lbph_pred_labels = []
-eigen_pred_labels = []
-
-# Open the video capture device
-cap = cv2.VideoCapture(0)
-
-while cap.isOpened():
-    # Read a frame from the video capture device
-    ret, frame = cap.read()
-
-    if not ret:
-        break
-
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the grayscale frame using the Haar Cascade classifier
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
-    # Loop over each detected face
-    for (x, y, w, h) in faces:
-        # Extract the face region from the grayscale frame
-        face_gray = gray[y:y+h, x:x+w]
-
-        # Resize the face region to the required size for the face recognition models
-        face_resized = cv2.resize(face_gray, (420, 420))
-
-        # Recognize the face using the LBPH model
-        lbph_label, lbph_confidence = lbph_recognizer.predict(face_resized)
-        label1_text = get_key(lbph_label, label_dict)
-        label1_text = re.sub(r'[\d_]+', '', label1_text)
-        lbph_pred_labels.append(lbph_label)
-
-        # Recognize the face using the Eigenfaces model face_recognizer
-        eigen_label, eigen_confidence = face_recognizer.predict(face_resized)
-        label_text = get_key(eigen_label, label_dict)
-        label_text = re.sub(r'[\d_]+', '', label_text)
-        eigen_pred_labels.append(eigen_label)
-
-        # Draw a rectangle around the detected face and display the predicted label and confidence
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame, f'LBPH: {lbph_label} ({lbph_confidence:.2f})', (x, y-20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.putText(frame, f'Eigenfaces: {eigen_label} ({eigen_confidence:.2f})', (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-        # Add the true label for the detected face to the list of true labels
-        true_labels.append(lbph_label)  # Replace 0 with the true label for the detected face
-
-    # Display the frame
-    cv2.imshow('Face Recognition', frame)
-
-    # Exit the loop if the 'q' key is pressed
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# Make sure the input variables have the same number of samples
-    if len(true_labels) == len(lbph_pred_labels):
-        # Calculate the accuracy, precision, recall, and F1 score for the LBPH model
-        lbph_accuracy = accuracy_score(true_labels, lbph_pred_labels)
-        lbph_precision = precision_score(true_labels, lbph_pred_labels, average='weighted')
-        lbph_recall = recall_score(true_labels, lbph_pred_labels, average='weighted')
-        lbph_f1_score = f1_score(true_labels, lbph_pred_labels, average='weighted')
-
-        # Calculate the accuracy, precision, recall, and F1 score for the Eigenfaces model
-        eigen_accuracy = accuracy_score(true_labels, eigen_pred_labels)
-        eigen_precision = precision_score(true_labels, eigen_pred_labels, average='weighted')
-        eigen_recall = recall_score(true_labels, eigen_pred_labels, average='weighted')
-        eigen_f1_score = f1_score(true_labels, eigen_pred_labels, average='weighted')
-
-        # Print the metrics for both models and select the best output label
-        if lbph_f1_score > eigen_f1_score:
-            print('LBPH model is better')
-            best_labels = lbph_pred_labels
-        else:
-            print('Eigenfaces model is better')
-            best_labels = eigen_pred_labels
-
-        print(f'LBPH accuracy: {lbph_accuracy:.2f}')
-        print(f'LBPH precision: {lbph_precision:.2f}')
-        print(f'LBPH recall: {lbph_recall:.2f}')
-        print(f'LBPH F1 score: {lbph_f1_score:.2f}')
-
-        print(f'Eigenfaces accuracy: {eigen_accuracy:.2f}')
-        print(f'Eigenfaces precision: {eigen_precision:.2f}')
-        print(f'Eigenfaces recall: {eigen_recall:.2f}')
-        print(f'Eigenfaces F1 score: {eigen_f1_score:.2f}')
-
-        # Print all predicted labels and confidence values
-        for i in range(len(best_labels)):
-            print(f'Frame {i+1}: LBPH: {lbph_pred_labels[i]} ({lbph_confidence:.2f}), Eigenfaces: {eigen_pred_labels[i]} ({eigen_confidence:.2f})')
-
-        # Do something with the best output labels
-        # print(best_labels)
-
-# Release the video capture device and close all windows
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import cv2
 import os
 import time
-
+import shutil
+import random
 
 def train():
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -20,9 +21,14 @@ def train():
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
+    # Set up save directory
+    testsave_dir = './test/'+name
+    if not os.path.exists(testsave_dir):
+        os.makedirs(testsave_dir)
+
     # Set up image counter
     img_count = 0
-    takesamples = 30
+    takesamples = 35
 
     # Capture images
     while True:
@@ -35,8 +41,8 @@ def train():
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
-    # Draw rectangles around the detected faces and save the images
-        if(img_count<=takesamples):
+        # Draw rectangles around the detected faces and save the images
+        if(img_count<=takesamples-5):
             for (x, y, w, h) in faces:
                 if(len(faces)<=1):
                     img_count += 1
@@ -45,6 +51,17 @@ def train():
                     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
                     print(f'Saved {img_name}')
                     time.sleep(0.5)
+        elif(img_count<=takesamples):
+                for (x, y, w, h) in faces:
+                    if(len(faces)<=1):
+                        img_count += 1
+                        img_name = name + "_" + str(img_count) + ".png"
+                        cv2.imwrite(os.path.join(testsave_dir, img_name), frame)
+                        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                        print(f'Saved {img_name}')
+                        time.sleep(0.5)
+
+            
         else:
             print("Done")
             break
@@ -53,7 +70,23 @@ def train():
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) == ord('q'):
             break
+    # Define the source and destination folders
+    source_folder = './candidates'
 
+    # Get a list of all image files in the source folder
+    image_files = [f for f in os.listdir(source_folder) if f.endswith('.jpg')]
+
+    # Choose a random image file from the list
+    random_image_file = random.choice(image_files)
+
+    # Construct the paths to the source and destination files
+    source_file = os.path.join(source_folder, random_image_file)
+    destination_file = os.path.join(testsave_dir, random_image_file)
+
+    # Copy the file from the source folder to the destination folder
+    shutil.copyfile(source_file, destination_file)
+
+    
     # Release camera and close window
     cap.release()
     cv2.destroyAllWindows()
